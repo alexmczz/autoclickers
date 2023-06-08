f89b11a7e2a474710f4d71f77d687b240ed673f6
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-06-07 20:54:35 -0400

    new features
    added time and duration

--- auto-clicker.py
+++ auto-clicker.py
@@ -1,7 +1,25 @@
+##!IMPORTANT!! YOU NEED TO PIP INSTALL PYAUTOGUI IN ORDER FOR THIS APP TO WORK!!!
+
+
 import pyautogui
 import time
 
+print("Enter Speed")
+usr_speed = input("> ")
+print("Enter Duration(Seconds are default. Type M for minutes S for seconds)")
+print("Example: M 1 S 30")
+usr_duration = input("> ")
+
+def total_time(times):
+  if "m" in times:
+    times = times.split(" ")
+    calculated_time = times[1] 
+    return calculated_time * 60
+  else:
+    return int(times)
+  
+  
 time.sleep(5)
-for i in range(100):
+for i in range(int(usr_speed)):
   pyautogui.click()
-  time.sleep(.1)
+  time.sleep(total_time(usr_duration))
