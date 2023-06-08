d6fbbf0f0a98fc52d06cd68d82ac2e5e4002f8a2
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-06-08 00:31:12 -0400

    more changes
    instead of local machine time keeping, i will be using datetime to grab internet time, add the time given by user and adding that to internet time. after the times are equal the timer loop will break

--- auto-clicker.py
+++ auto-clicker.py
@@ -1,9 +1,9 @@
 ##!IMPORTANT!! YOU NEED TO PIP INSTALL PYAUTOGUI IN ORDER FOR THIS APP TO WORK!!!
 
-
+import os
 import pyautogui
-import time
-
+import time 
+from datetime import datetime
 
 def res_inp():
   print("Enter Speed")
@@ -12,7 +12,7 @@ def res_inp():
   print("Example: M 1 S 30")
   usr_duration = input("> ")
 
-def get_time():
+def get_duration():
   res = input("test [string][int] > ")
   if "M" in res:
     times = [res.split()]
@@ -26,21 +26,42 @@ def get_time():
       #print(test3)
 
 
+def rate():
+  res = input("Enter rate(clicks per second)> ")
+  rate_i = int(res)
+  return rate_i
+  
 
 
 def clicker():
-  duration = get_time()
+  duration = get_duration()
   counter = 0
-  for i in range(duration):
-     
-     while counter <= duration:
-      counter += 1
-      pyautogui.click()
-      #print(f"click: {counter}")
+  
+  my_rate = rate()
+  
+  while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    
+    dif_time = current_time + my_rate
+    
+    if str(current_time) != str(dif_time):
+      print(current_time)
+      #os.system("clear")
+      continue
+    else:
+      break
     
 
 
 
 
 
-clicker()
+
+
+  
+  
+
+
+#clicker()
+
