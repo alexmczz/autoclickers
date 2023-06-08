c631b1cb6977f8aed2b94f0a739bfcfdb327cde3
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-06-07 23:48:45 -0400

    restructure
    Decided to restructure duration/time and rate of click functions. each will return calculated numbers for both time(duration) inputs and rate

--- auto-clicker.py
+++ auto-clicker.py
@@ -4,22 +4,43 @@
 import pyautogui
 import time
 
-print("Enter Speed")
-usr_speed = input("> ")
-print("Enter Duration(Seconds are default. Type M for minutes S for seconds)")
-print("Example: M 1 S 30")
-usr_duration = input("> ")
 
-def total_time(times):
-  if "m" in times:
-    times = times.split(" ")
-    calculated_time = times[1] 
-    return calculated_time * 60
-  else:
-    return int(times)
-  
-  
-time.sleep(5)
-for i in range(int(usr_speed)):
-  pyautogui.click()
-  time.sleep(total_time(usr_duration))
+def res_inp():
+  print("Enter Speed")
+  usr_speed = input("> ")
+  print("Enter Duration(Seconds are default. Type M for minutes S for seconds)")
+  print("Example: M 1 S 30")
+  usr_duration = input("> ")
+
+def get_time():
+  res = input("test [string][int] > ")
+  if "M" in res:
+    times = [res.split()]
+    times2 = []
+    for char in times[0]:
+      times2.append(char)
+    #print(f"test 1: {test}")
+    #print(f"test 2: {test2}")
+    #print(f"test 3: {test2[1]}")
+    return int(times2[1])
+      #print(test3)
+
+
+
+
+def clicker():
+  duration = get_time()
+  counter = 0
+  for i in range(duration):
+     
+     while counter <= duration:
+      counter += 1
+      pyautogui.click()
+      #print(f"click: {counter}")
+    
+
+
+
+
+
+clicker()
